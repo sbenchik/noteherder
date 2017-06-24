f4804c41fcd66e14a5d21ba5a9751c854b26e289
Steve Benchik <stevebenchik@gmail.com>
2017-06-23 22:17:50 -0400

    Add main component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 
 import './App.css';
-import Sidebar from './Sidebar'
-import NoteList from './NoteList'
-import NoteForm from './NoteForm'
+import Main from './Main'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Sidebar />
-        <NoteList />
-        <NoteForm />
+        <Main />
       </div>
     );
   }
---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Sidebar from './Sidebar'
+import NoteList from './NoteList'
+import NoteForm from './NoteForm'
+
+const Main = () => {
+    return (
+        <div className="Main">
+            <Sidebar />
+            <NoteList />
+            <NoteForm />
+        </div>
+    )
+}
+
+export default Main;
