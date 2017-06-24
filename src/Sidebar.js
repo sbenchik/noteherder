26af15e5b7b28edf6e21360b001523921751dfdc
Steve Benchik <stevebenchik@gmail.com>
2017-06-23 22:46:41 -0400

    Load images locally

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,21 @@
 import React from 'react'
 
+import quill from './quill.svg'
+import newHover from './new-hover.png'
+import newImg from './new.png'
+
 const Sidebar = () => {
-    return <p>SIDEBAR</p>
+    return (
+    <nav class="Sidebar">
+        <div class="logo">
+            <img src={quill} alt="Noteherder" />
+        </div>
+        <button class="new-note">
+            <img src={newHover} alt="New note" />
+            <img class="outline" src={newImg} alt="New note" />
+        </button>
+    </nav>
+    )
 }
 
 export default Sidebar
